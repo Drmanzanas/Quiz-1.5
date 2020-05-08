56bdbad3ed42972ea1857bf33c422544e8fcb5c1
Ulises Menem <ulisesmenem@MBP-de-Ulises.technicolor.net>
2020-05-08 17:20:31 +0200

    terminado CSS, falta validar

--- scripts.js
+++ scripts.js
@@ -1,17 +1,45 @@
+let $fieldset = document.getElementsByTagName('fieldset');
+
+function cambiarTag(array) {
+    let arraySection = [];
+    for(let i = 0; i < array.length; i++) {
+        let $section = document.createElement('section');
+        $section.innerHTML = array[i].innerHTML;
+        arraySection[i] = $section;
+        array[i].parentNode.replaceChild(arraySection[i],array[i])
+    }
+    let i = 0;
+    while(i < arraySection.length){
+        arraySection[i].className = 'quiz-fieldset';
+        i++
+    }
+}
+
+cambiarTag($fieldset)
+cambiarTag($fieldset)
+cambiarTag($fieldset)
+
 let $flechita = document.querySelector('span');
 
 $flechita.addEventListener('click', function(event) {
     event.preventDefault();
-    document.getElementsByTagName('main').style.top = '-100vh';
+    window.location.href = "#comienzo";
 })
 
 let $button = document.querySelectorAll('button[type="button"]');
 
-function subiendoATodos(array, func) {
-    for(let i = 0; i < array.length; i++){
-        array[i].addEventListener('click',func)
-
+function subirForm(array) {
+    let $form = document.getElementsByTagName('form')[0];
+    for(let i = 0; i < array.length; i++) {
+        array[i].addEventListener('click',function() {
+            console.log(array[i], i)
+            if(i == 0) {
+               return $form.style.top = `-${i + 7}00px`;
+            } else {
+                 return $form.style.top = '-' + (i * 7) + '00px';
+            }
+        })
     }
 }
 
-subiendoATodos($button, subirForm)
+subirForm($button)
